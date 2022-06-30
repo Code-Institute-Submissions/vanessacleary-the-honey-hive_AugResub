b9f259e4e60a1251c60759e965e79fb42a20ae14
vanessacleary <vanessacleary@yahoo.com>
2022-06-30 22:19:17 +0000

    Added admin class

---
+++ contact/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    """ Outlines what fields we want admin to see """
+    list_display = (
+        'full_name',
+        'email_from',
+        'order_number',
+        'enquiry',
+        'enquiry_sent',
+    )
+    ordering = ('-enquiry_sent',)
+
+
+admin.site.register(Contact, ContactAdmin)
