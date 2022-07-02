9c8f2049846f821da826083439aed02fe6da3584
vanessacleary <vanessacleary@yahoo.com>
2022-07-02 14:56:59 +0000

    Added validation logic to news letter form

--- letter/views.py
+++ letter/views.py
@@ -2,3 +2,17 @@ from django.shortcuts import render
 from . forms import SubscibersForm, MailMessageForm
 
 # Create your views here.
+
+def letter(request):
+      if request.method == 'POST':
+        form = SubscibersForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Subscription Successful')
+            return redirect('/')
+    else:
+        form = SubscibersForm()
+    context = {
+        'form': form,
+    }
+  return render(request, 'letter.html', context)
