506328e3b770c6db7998d08801d9fadec7b8c5f8
vanessacleary <vanessacleary@yahoo.com>
2022-07-03 21:30:46 +0000

    Removed secret key and set debug

--- the_honey_hive/settings.py
+++ the_honey_hive/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i&a^ut1dapftbn9vgq5s7piu7ifa04pujxz&-wp8dcxhjmt!j$'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-honey-hive.heroku.com', 'local host']
 
